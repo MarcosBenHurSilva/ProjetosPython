6344a20dafdaf68d1ef205a4a92c13de36048e5b
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-03 22:50:27 -0300

    First hour of Python tutorial

---
+++ multiple_assignment.py
@@ -0,0 +1,11 @@
+# multiple assignment = allows us to assign multiple variables at the same time in one line of code
+
+# name = "Marcos"
+# age = 31
+# attractive = True
+
+name, age, attractive = "Marcos", 31, True
+
+print(name)
+print(age)
+print(attractive)
---
+++ string_methods.py
@@ -0,0 +1,17 @@
+name = "Marcos Silva"
+name1 = "benhur"
+name2 = "DORNELES"
+name3 = "123456"
+name4 = "Silva"
+
+
+print(len(name))
+print(name.find("o"))
+print(name1.capitalize())
+print(name1.upper())
+print(name2.lower())
+print(name3.isdigit())
+print(name4.isalpha())
+print(name.count("a"))
+print(name.replace("o","u"))
+print(name*3)
