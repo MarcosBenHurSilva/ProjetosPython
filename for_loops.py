9c63065b73e5c3c308916630b872d3c265eb20e6
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-04 21:18:11 -0300

    Add Countdown

--- for_loops.py
+++ for_loops.py
@@ -5,12 +5,19 @@ import time
 #            while loop = unlimited
 #            for loop = limited
 
+#for i in range(10):
+#     print(i)
+
 # for i in range(10):
 #     print((i+1))
 
 # for i in range(50,100+1,2):
 #     print(i)
 
-for i in "Marcos Silva":
-    print(i)
+# for i in "Marcos Silva":
+#     print(i)
 
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy New Year!")
