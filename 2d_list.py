c9277d2affa89872514978c298bfff1e3bc66849
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-05 00:25:47 -0300

    Atualizar

---
+++ 2d_list.py
@@ -0,0 +1,8 @@
+# 2D List = a list of lists
+drinks = ["coffee", "soda", "tee"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
+
+print(food)
---
+++ list.py
@@ -0,0 +1,19 @@
+# list = used to store multiple items in a single variable
+
+food = ["pizza", "hamburger", "hotdog", "spaghetti", "pudding"]
+
+# print(food)
+
+# print(food[0])
+
+food[0] = "sushi"
+
+# food.append("ice cream")
+# food.remove("hotdog")
+# food.pop()
+# food.insert(0, "cake")
+# food.sort()
+# food.clear()
+
+for x in food:
+    print(x)
