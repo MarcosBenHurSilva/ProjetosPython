428bc2418cd00ff81080378ec15f08c911271de9
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-10 00:49:27 -0300

    Continuar a partir da aula 87

--- progress_bar.py
+++ progress_bar.py
@@ -1,5 +1,36 @@
+import time
 from tkinter import *
+from tkinter.ttk import *
+
+
+def start():
+    GB = 500
+    download = 0
+    speed = 1
+    while download < GB:
+        time.sleep(0.05)
+        bar['value'] += (speed/GB)*100
+        download += speed
+        percent.set(str(int((download/GB)*100))+"%")
+        text.set(str(download) + "/" + str(GB) + " GB completed")
+        window.update_idletasks()
+
 
 window = Tk()
+
+percent = StringVar()
+text = StringVar()
+
+bar = Progressbar(window, orient=HORIZONTAL, length=300)
+bar.pack(pady=10)
+
+percent_label = Label(window, textvariable=percent)
+percent_label.pack()
+
+task_label = Label(window, textvariable=text)
+task_label.pack()
+
+button = (Button(window, text="download", command=start))
+button.pack()
 
 window.mainloop()
--- window_tab.py
+++ window_tab.py
@@ -3,6 +3,16 @@ from tkinter import ttk
 
 window = Tk()
 
-notebook = ttk.Notebook(window)  # widget that manages a collection
+notebook = ttk.Notebook(window)  # widget that manages a collection of windowa/displays
+
+tab1 = Frame(notebook)  # new frame for tab 1
+tab2 = Frame(notebook)  # new frame for tab 2
+
+notebook.add(tab1, text="Tab 1")
+notebook.add(tab2, text="Tab 2")
+notebook.pack(expand=True, fill="both")  # expand = expand to fill any space not otherwise occupied
+# fill = fill space on x and y axis
+Label(tab1, text="Hello, this is tab#1", width=50, height=25, font=("Arial", 25), bg='light green', fg='black').pack()
+Label(tab2, text="Hello, this is tab#2", width=50, height=25, font=("Arial", 25), bg='black', fg='light green').pack()
 
 window.mainloop()
