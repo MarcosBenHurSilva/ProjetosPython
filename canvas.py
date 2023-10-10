6f1cf8ea9145a344404e12506525244cd950f9f7
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-10 00:53:24 -0300

    update Canvas

--- canvas.py
+++ canvas.py
@@ -13,9 +13,9 @@ canvas = Canvas(window, height=500, width=500)
 # canvas.create_polygon(points,fill="yellow",outline="black",width=5)
 # canvas.create_arc(0,0,500,500,style=PIESLICE,start=270,width=5)
 
-# canvas.create_arc(0, 0, 500, 500, fill="red", extent=180, width=10)
-# canvas.create_arc(0, 0, 500, 500, fill="white", extent=180, start=180, width=10)
-# canvas.create_oval(190, 190, 310, 310, fill="white", width=10)
+canvas.create_arc(0, 0, 500, 500, fill="red", extent=180, width=10)
+canvas.create_arc(0, 0, 500, 500, fill="white", extent=180, start=180, width=10)
+canvas.create_oval(190, 190, 310, 310, fill="white", width=10)
 
 canvas.pack()
 
