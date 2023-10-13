5a77008496e54dadc390b313d2bf510079e94a07
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-10-13 16:18:19 -0300

    "Corrigito erro da classe food onde as coordenadas de spawn eram dadas em float ao invés de int"

--- snake_game.py
+++ snake_game.py
@@ -1,10 +1,10 @@
 from tkinter import *
 import random
 
-GAME_WIDTH = 1000
+GAME_WIDTH = 700
 GAME_HEIGHT = 700
 SPEED = 100
-SPACE_SIZE = 50
+SPACE_SIZE = 25
 BODY_PARTS = 3
 SNAKE_COLOR = "#00FF00"
 FOOD_COLOR = "#FF0000"
@@ -25,8 +25,8 @@ class Snake:
 
 class Food:
     def __init__(self):
-        x = random.randint(0, (GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
-        y = random.randint(0, (GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
+        x = random.randint(0, int(GAME_WIDTH / SPACE_SIZE) - 1) * SPACE_SIZE
+        y = random.randint(0, int(GAME_HEIGHT / SPACE_SIZE) - 1) * SPACE_SIZE
         self.coordinates = [x, y]
         canvas.create_oval(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
